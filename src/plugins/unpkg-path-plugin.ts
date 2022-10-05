bdd734cb17bbd161ebbd6cf6915b20689ee1a704
khatuna120 <shaveshovak@gmail.com>
2022-10-05 15:11:15 +0200

    feat: changes the sizes of the images

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -15,7 +15,10 @@ export const unpkgPathPlugin = () => {
         if(args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(
+              args.path, 
+              'https://unpkg.com' + args.resolveDir + '/')
+              .href
           }
         }
 
@@ -32,17 +35,20 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              const react = require('react');
+              const reactDom = require('react-dom')
+              console.log(react, reactDom);
             `,
           };
         } 
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        console.log(request);
 
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         }
       });
     },
