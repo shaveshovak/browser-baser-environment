289ea5ad1eb0d0996e5b8113a4f5a70418eb0ea1
khatuna120 <shaveshovak@gmail.com>
2022-10-13 11:34:47 +0200

    fix: resolved typescript error

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,19 @@
 
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache'
+});
+
+(async () => {
+  await fileCache.setItem('color', 'red');
+
+  const color = await fileCache.getItem('color');
+
+  console.log(color)
+}) ()
      
 export const unpkgPathPlugin = () => {
   return {
@@ -42,14 +55,28 @@ export const unpkgPathPlugin = () => {
           };
         } 
 
+        // Check to see if we have already fetched this file and if it is in the cache, if it is, return it immediately
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if(cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
+
+        // store response in cache 
         console.log(request);
 
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
