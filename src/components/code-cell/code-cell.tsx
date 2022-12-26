b0509cc7b25581e01bf839690a5fcea7bf0e3e44
khatuna120 <shaveshovak@gmail.com>
2022-12-26 17:47:04 +0100

    added changes for resizing/windows

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -1,25 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import bundler from '../../bundler';
 import CodeEditor from '../code-editor/code-editor';
 import Preview from '../preview/preview';
 import Resizable from '../resizable/resizable';
 import './code-cell.css';
 
 const CodeCell = () => {
-
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundler(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction='vertical'>
       <div className='main-container'>
-      <CodeEditor 
-          initialValue='const a = 1'
-          onChange={(value) => setInput(value)}
-      />
-      <Preview code={code} />
+        <Resizable direction='horizontal'>
+          <CodeEditor 
+            initialValue='const a = 1'
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
       </div>
     </Resizable>
   )
-}
+} 
 
 export default CodeCell;
--- src/components/resizable/resizable.tsx
+++ src/components/resizable/resizable.tsx
@@ -1,4 +1,5 @@
-import { ResizableBox } from 'react-resizable';
+import { useEffect, useState } from 'react';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -7,13 +8,60 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return <ResizableBox 
-    width={Infinity} 
-    height={300}
-    maxConstraints={[ Infinity, window.innerHeight * 0.9]}
-  >
+  let resizeableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75); 
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if(timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if(window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      });
+     };
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, [width]);
+
+  if (direction === 'horizontal') {
+    resizeableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      width,
+      height: Infinity,
+      resizeHandles: ['e'],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      }
+    };
+  } else {
+    resizeableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [ Infinity, innerHeight * 0.9],
+      width: Infinity,
+      height: 300,
+      resizeHandles: ['s']
+    };
+  }
+  return ( 
+    <ResizableBox 
+      {...resizeableProps}
+    >
       {children}
     </ResizableBox>
+  )
 }
 
 export default Resizable;
