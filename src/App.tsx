7f5018077f4f0c5efcca46c53c4d0db427d42ed4
khatuna120 <shaveshovak@gmail.com>
2023-01-13 00:27:36 +0100

    feat: added design patterns for redux with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import TextEditor from './components/text-editor/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   )
 }
 
