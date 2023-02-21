40021f3232bece73327dae142be5671d08aade38
khatuna120 <shaveshovak@gmail.com>
2023-02-21 11:33:11 +0100

    added buttons for adding celss

---
+++ src/components/add-cell/add-cell.tsx
@@ -0,0 +1,37 @@
+import './add-cell.css';
+import { useActions } from '../../hooks/use-action';
+
+interface AddCellProps {
+    nextCellId: string | null;
+    forceVisible?: boolean;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
+    const { insertCellBefore } = useActions();
+
+    return <div className={`add-cell ${forceVisible && 'force-visible'}`}>
+        <div className='add-buttons'>
+            <button 
+                onClick={() => insertCellBefore(nextCellId, 'code')}
+                className='button is-rounded is-primary is-small'
+            >
+                <span className='icon is-small'>
+                    <i className='fas fa-plus'></i>
+                </span>
+                <span>Code</span>
+            </button>
+            <button 
+                onClick={() => insertCellBefore(nextCellId, 'text')}
+                className='button is-rounded is-primary is-small'
+            >
+                <span className='icon is-small'>
+                    <i className='fas fa-plus'></i>
+                </span>
+                <span>Text</span>
+            </button>
+        </div>
+        <div className='divider'></div>
+    </div>
+}
+
+export default AddCell;
--- src/components/cell-list/cell-list.tsx
+++ src/components/cell-list/cell-list.tsx
@@ -1,12 +1,25 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../../hooks/use-typed.selector";
 import CellListItem from "./cell-list-item";
+import AddCell from "../add-cell/add-cell";
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
 
-    const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell} />);
+    const renderedCells = cells.map(cell => 
+                    <Fragment key={cell.id}>
+                        <AddCell nextCellId={cell.id} />
+                        <CellListItem cell={cell} />
+                    </Fragment>
+                );
 
-    return <div>{renderedCells}</div>
+    return <div>
+            {renderedCells}
+            <AddCell 
+                forceVisible={cells.length === 0} 
+                nextCellId={null} 
+            />
+        </div>
 }
 
 export default CellList;
